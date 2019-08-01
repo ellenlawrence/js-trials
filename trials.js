e93926d1c94172ffe52cfe0b9f60a6578c02643b
Hackbright Student <no-reply@hackbrightacademy.com>
2019-08-01 12:56:40 -0700

    Completed showPhoneNums function

--- trials.js
+++ trials.js
@@ -11,19 +11,43 @@ const addresses = ["123 Main Street", "683 Sutter Street", "1600 Pennsylvania Av
 const phoneNumbers = new Map();
 
 // Add some phone numbers to our map
-
+phoneNumbers.set('home','510-867-5309');
+phoneNumbers.set('mobile', '415-555-1212');
+phoneNumbers.set('business', '415-123-4567');
 
 
 // ///////////////////////////////////////////////////////
 // Create User Info Display:
+function showAddresses(addresses) {
+  console.log(`\
+Addresses:
+${addresses[0]}
+${addresses[1]}
+${addresses[2]}\
+  `);
+}
 
 // Add function to print account information 
+function printAccountInfo(name, number, businessName) {
+  console.log(`\
+    Account Holder Name: ${name}
+    Account Holder Number: ${number}
+    Business Name: ${businessName}\
+  `);
+}
 
 
 // Add function to print all addresses, including a header
 
 
 // Add function to print phone types and numbers
+function showPhoneNums(phoneNumbers) {
+  console.log('Phone Numbers:');
+  for(const [key, val] of phoneNumbers) {
+      console.log(`${key}: ${val}`);
+  }  
+}
+
 
 
 // ///////////////////////////////////////////////////////
